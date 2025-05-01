1326afa4914ef8b1ecbbe1a01855e7d41cb2c83f
MilanIsac <milan.isac.7@gmail.com>
2025-05-01 23:02:09 +0530

    ffmpeg functionality added

--- server/services/notifications.services.js
+++ server/services/notifications.services.js
@@ -2,17 +2,46 @@ const MLClient = require("../services/mlClient.services");
 const Camera = require("../models/camera.models");
 const twilio = require("twilio");
 
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+
+const client = new twilio(accountSid, authToken);
+
 async function processCameraFree(req, res) {
     try {
         const { cameraId } = req.body;
+        const camera = await Camera.findById({ cameraId });
+        if(!camera){
+            return res.status(400).json({ 'Error' : 'Camera not found' });
+        }
         const imagePath = "path";
         const prediction = await MLClient.predictDisaster(imagePath);
         res.json(prediction);
     }
     catch(error) {
         res.status(500).json({
-            error: "Failed to process camera feed"})
+            error: "Failed to process camera feed"
+        });
     }
 }
 
-module.export = { processCameraFree };
+async function sendSMS({ message, recipients }){
+    try{
+        for(const phoneNumber of recipients){
+            await client.messages.create({
+                body : message,
+                from : twilioNumber,
+                to : phoneNumber
+            })
+            console.log(`SMS sent to ${phoneNumber}`);
+        }
+    }
+    catch(error){
+        console.error("Error sending messages : ", error.message);
+        throw error;
+    }
+}
+
+module.export = { processCameraFree, sendSMS };
